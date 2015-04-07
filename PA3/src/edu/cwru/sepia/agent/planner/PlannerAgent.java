491105ff548cf97d0a5b27168526738336aeda8c
Andrew Mason <mason@case.edu>
2015-04-07 15:44:47 -0400

    import cleanup

--- PA3/src/edu/cwru/sepia/agent/planner/PlannerAgent.java
+++ PA3/src/edu/cwru/sepia/agent/planner/PlannerAgent.java
@@ -4,9 +4,7 @@ import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.agent.Agent;
 import edu.cwru.sepia.agent.planner.actions.StripsAction;
 import edu.cwru.sepia.environment.model.history.History;
-import edu.cwru.sepia.environment.model.state.ResourceNode;
 import edu.cwru.sepia.environment.model.state.State;
-import edu.cwru.sepia.environment.model.state.Unit;
 
 import java.io.*;
 import java.util.*;
