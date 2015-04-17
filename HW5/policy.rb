88e50c90e02adad55c7856325b28eabd325c53fe
Andrew Mason <mason@case.edu>
2015-04-16 22:57:44 -0400

    policy iteration swag

--- HW5/policy.rb
+++ HW5/policy.rb
@@ -1,17 +1,19 @@
 $fsm = {
-	:s1 => {  :s1 => {:a => 0.2, :b => 0.9},
-		  :s2 => {:a => 0.8, :b => 0.0},
-                  :s3 => {:a => 0.0, :b => 0.1}
-		},
-        :s2 => {  :s1 => {:a => 0.8, :b => 0.0},
-		  :s2 => {:a => 0.2, :b => 0.9},
-		  :s3 => {:a => 0.0, :b => 0.1}
-		},
+	:s1 => {
+    :s1 => {:a => 0.2, :b => 0.9},
+    :s2 => {:a => 0.8, :b => 0.0},
+    :s3 => {:a => 0.0, :b => 0.1}
+  },
+  :s2 => {
+    :s1 => {:a => 0.8, :b => 0.0},
+    :s2 => {:a => 0.2, :b => 0.9},
+    :s3 => {:a => 0.0, :b => 0.1}
+  },
 	:s3 => {
-		  :s1 => {:a => 0, :b => 0},
-		  :s2 => {:a => 0, :b => 0},
-		  :s3 => {:a => 1, :b => 1}
-		}
+    :s1 => {:a => 0, :b => 0},
+    :s2 => {:a => 0, :b => 0},
+    :s3 => {:a => 1, :b => 1}
+  }
 }
 $r = {:s1 => -1, :s2 => -2, :s3 => 0}
 $states = $fsm.keys
@@ -26,14 +28,13 @@ def prob(state, target, action)
 end
 
 def q_value(s, action, gamma, v)
-  $states.map{|s_prime| prob(s, s_prime, action)*$r[s_prime] +	gamma * v[s_prime]}.reduce(:+)
+  $states.map{|s_prime| prob(s, s_prime, action)*($r[s_prime] +	gamma * v[s_prime])}.reduce(:+)
 end
 
 def policy_iterate(policy,initial_values, gamma)
-  v, change = initial_values, false
-  until change
-    p policy
-    change = false
+  v, no_change = initial_values, false
+  until no_change
+    no_change = true
     $states.each do |s|
       v[s] = q_value(s, policy[s], gamma, v)
     end
@@ -44,7 +45,7 @@ def policy_iterate(policy,initial_values, gamma)
         if q > q_best
           policy[s] = action
           q_best = q
-          change = true
+          no_change = false
         end
       end
     end
@@ -52,4 +53,8 @@ def policy_iterate(policy,initial_values, gamma)
   return policy
 end
 
-p policy_iterate({:s1 => :b, :s2 => :b}, {:s1 => -1, :s2 => -2, :s3 => 0}, 1)
+# part ii
+p policy_iterate({:s1 => :b, :s2 => :b}, {:s1 => -9, :s2 => -18, :s3 => 0}, 1)
+
+# part iii
+# does not compute
