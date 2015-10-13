0f2654d3cbe0afc867fb15324d41e8fa44248178
Andrew Mason <mason@case.edu>
2015-10-12 21:31:35 -0400

    dropping in programming assignment 3 from blackboard

---
+++ eecs440/pa3/nbayes.py
@@ -0,0 +1,24 @@
+"""
+The Naive Bayes Classifier
+"""
+import numpy as np
+import scipy
+
+class NaiveBayes(object):
+
+
+    def __init__(self, alpha=0):
+        """
+        Constructs a Naive Bayes classifier
+
+        @param m : Smoothing parameter (0 for no smoothing)
+        """
+
+    def fit(self, X, y):
+        pass #add code here
+
+    def predict(self, X):
+        pass  #add code here
+
+    def predict_proba(self, X):
+        pass  #add code here
