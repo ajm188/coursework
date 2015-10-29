57e21c6b5b4d0987077aad39db1fd76a33dabc10
Andrew Mason <mason@case.edu>
2015-10-29 10:46:50 -0400

    comments

--- eecs440/pa3/logistic_regression.py
+++ eecs440/pa3/logistic_regression.py
@@ -16,16 +16,27 @@ from folds import get_folds
 
 
 def sigmoid(x):
+    """
+    Computes the sigmoid of x.
+    If x is an ndarray, computes the sigmoid element-wise.
+    """
     return (np.exp(-x) + 1) ** (-1)
 
 
 def objective_func(w_and_b, X, y, _lambda):
+    """
+    The objective function we are trying to minimize for logistic regression.
+    """
     w, b = w_and_b[0:-1], w_and_b[-1]
     s = sigmoid(y * (np.dot(X, w) + b))
     return _lambda * 0.5 * (np.linalg.norm(w) ** 2) + np.sum(np.log(s ** (-1)))
 
 
 def gradient(w_and_b, X, y, _lambda):
+    """
+    Gradient of the objective function we are trying to minimize for logistic
+    regression.
+    """
     w, b = w_and_b[0:-1], w_and_b[-1]
     s = sigmoid((-y) * (np.dot(X, w) + b))
     del_common = (-y) * s
@@ -35,6 +46,10 @@ def gradient(w_and_b, X, y, _lambda):
 
 
 def random_weights(dimensions, r):
+    """
+    Returns an ndarray with the given dimensions, where each value is between
+    r[0] and r[1].
+    """
     lower, upper = r
     size = upper - lower
     offset = lower
@@ -54,11 +69,18 @@ class LogisticRegression(object):
         self._lambda = kwargs.pop('lambda')
 
     def fit(self, X, y):
+        """
+        Fit a classifier to X and y. Performs parameter tuning for the choice
+        of λ.
+        """
         self._lambda = self.tune(X, y, [0, 0.001, 0.01, 0.1, 1, 10, 100])
         print('Chose {} for λ'.format(self._lambda))
         self._fit(X, y)
 
     def _fit(self, X, y):
+        """
+        Helper routine that fits X and y according to whatever self._lambda is.
+        """
         np.seterr(over='ignore', divide='ignore')
         self._enable_unnominalization(X)
         X = self.unnominalize(X)
@@ -81,6 +103,10 @@ class LogisticRegression(object):
         self.w, self.b = res.x[0:-1], res.x[-1]
 
     def tune(self, X, y, lambda_range):
+        """
+        Perform internal cross-fold validation to find the λ which maximizes
+        AUC.
+        """
         folds = get_folds(X, y, 5)
         AUCs = []
         for _lambda in lambda_range:
@@ -97,9 +123,16 @@ class LogisticRegression(object):
         return lambda_range[np.argmax(AUCs)]
 
     def normalize(self, X):
+        """
+        Normalizes X according to a set of means and standard deviations.
+        """
         return (X - self.means) / self.stddevs
 
     def _enable_unnominalization(self, X):
+        """
+        Sets up an instance variable that will be used later to transform
+        discretely-valued attributes into continuous attributes.
+        """
         for i, _ in enumerate(self.schema.feature_names):
             if self.schema.is_nominal(i):
                 self.nominals[i] = \
@@ -107,6 +140,9 @@ class LogisticRegression(object):
                           for j, v in enumerate(self.schema.nominal_values[i])])
 
     def unnominalize(self, X):
+        """
+        Transforms X from discretely-valued to continuous attributes.
+        """
         D = np.empty_like(X)
         for i, _ in enumerate(self.schema.feature_names):
             X_C = X[:, i]
@@ -119,6 +155,10 @@ class LogisticRegression(object):
         return D
 
     def predict(self, X):
+        """
+        Makes predictions about X.
+        Returns 1 if (w dot x) + b > 0 and -1 otherwise for each x in X.
+        """
         np.seterr(over='ignore')
         X = self.normalize(self.unnominalize(X))
         predictions = np.dot(X, self.w) + self.b
@@ -127,6 +167,9 @@ class LogisticRegression(object):
         return predictions
 
     def predict_proba(self, X):
+        """
+        Returns p(y=1|x) for each x in X.
+        """
         np.seterr(over='ignore', divide='ignore')
         X = self.normalize(self.unnominalize(X))
         dot_prods = -(np.dot(X, self.w[:, np.newaxis]) + self.b)
--- eecs440/pa3/nbayes.py
+++ eecs440/pa3/nbayes.py
@@ -13,22 +13,25 @@ import stats
 from folds import get_folds
 
 
-def fancy_print(i):
-    print('\b' * len(str(abs(i - 1))), end='')
-    print(i, end='')
-    sys.stdout.flush()
-
-
 class NaiveBayes(object):
 
     NUM_BINS = 10
 
     def __init__(self, m=None, schema=None):
+        """
+        Initialize a NaiveBayes classifier with m and schema.
+        m is generally ignored, in favor of doing parameter tuning.
+        """
         self.m = m
         self.schema = schema
         self.discretizations = {}
 
     def fit(self, X, y):
+        """
+        Fit a classifier to the training data X and y.
+        Does internal cross-fold validation to tune the m parameter for doing
+        m-estimates.
+        """
         self._enable_discretization(X)
         X = self.discretize(X).astype('int')
         m = self.tune(X, y, [0, 0.001, 0.01, 0.1, 1, 10, 100])
@@ -37,6 +40,12 @@ class NaiveBayes(object):
         self._fit(X, y)
 
     def _fit(self, X, y):
+        """
+        Does the actual fitting. Assumes the data has already been transformed
+        from continuous attributes into discrete ones.
+
+        Performs m-estimate fitting.
+        """
         pos_y, neg_y = np.where(y == 1)[0], np.where(y == -1)[0]
         pos_X, neg_X = X[pos_y], X[neg_y]
 
@@ -66,6 +75,13 @@ class NaiveBayes(object):
         self.y_prob = len(pos_y) / len(y)
 
     def tune(self, X, y, m_range):
+        """
+        Performs parameter tuning on m. Uses internal cross-fold validation.
+        Assumes the training data has already been transformed into discrete
+        values, since this operation is expensive.
+
+        Chooses the m which maximizes AUC.
+        """
         folds = get_folds(X, y, 5)
         AUCs = []
         for m in m_range:
@@ -81,10 +97,18 @@ class NaiveBayes(object):
         return m_range[np.argmax(AUCs)]
 
     def predict(self, X):
+        """
+        Make predictions on X after converting X into discretely-valued
+        attributes.
+        """
         X = self.discretize(X).astype('int')
         return self._predict(X)
 
     def _predict(self, X):
+        """
+        The actual prediction code. Assumes that X is discretely-valued.
+        Returns 1 if p(Y=1,x) > p(Y=-1,x) and -1 otherwise for each x in X.
+        """
         pos, neg = [self.predict_p(X, p)
                     for p in [self.pos_probs, self.neg_probs]]
         pos_probs = pos * self.y_prob
@@ -93,17 +117,30 @@ class NaiveBayes(object):
         return np.where(pos_probs > neg_probs, ones, ones - 2)
 
     def predict_proba(self, X):
+        """
+        Return p(y=1,x) for each x in X.
+        """
         X = self.discretize(X).astype('int')
         return self._predict_proba(X)
 
     def _predict_proba(self, X):
+        """
+        Returns p(y=1,x) for each x in X. Assumes X is discretely-valued.
+        """
         return self.predict_p(X, self.pos_probs) * self.y_prob
 
     def predict_p(self, X, probs):
+        """
+        Returns p(y=y',x) for each p' in probs, x in X.
+        """
         row_vec = np.arange(len(probs))
         return np.array([np.product(probs[row_vec, x]) for x in X])
 
     def _enable_discretization(self, X):
+        """
+        Sets up an instance variable that will be used later to transform
+        continuous attributes into discretely-valued attributes.
+        """
         for i, _ in enumerate(self.schema.feature_names):
             if not self.schema.is_nominal(i):
                 self.discretizations[i] = []
@@ -116,6 +153,10 @@ class NaiveBayes(object):
                     ub += bin_size
 
     def discretize(self, X):
+        """
+        Transforms X from continuous attributes into discretely-valued
+        attributes.
+        """
         D = np.empty_like(X)
         for i, _ in enumerate(self.schema.feature_names):
             X_C = X[:, i]
